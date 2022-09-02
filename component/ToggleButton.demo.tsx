f96291cc0526d58d9d787ae3f6e401521dcfbcbe
subin <subin9192@gmail.com>
2022-09-02 17:17:38 +0900

    feat: ToggleButton

---
+++ component/ToggleButton.demo.tsx
@@ -0,0 +1,33 @@
+import { demo } from "frontbook-react";
+
+export default demo({
+  // * 컴포넌트가 패널로 보여질 가로 크기입니다. (최대 크기는 12입니다.)
+  w: 6,
+
+  // * 컴포넌트가 패널로 보여질 세로크기입니다.
+  h: 6,
+
+  // * 컴포넌트 Props 들을 명시하는 공간입니다.
+  controls: {
+    // * 문자열 예시
+    // * 숫자 예시
+    // age: {
+    //   type: "number",
+    //   defaultValue: 0,
+    // },
+
+    // * 선택 가능한 문자열 예시
+    // language: {
+    //   type: "select",
+    //   defaultValue: {
+    //     defaultValue: "English",
+    //     selectableValues: ["English", "Korean"],
+    //   },
+    // },
+  },
+
+  renderProps: (props) => {
+    // (고급) 여기서 Props 를 중간에 자바스크립트로 바꿀 수 있습니다.
+    return props;
+  },
+});
---
+++ component/ToggleButton.tsx
@@ -0,0 +1,34 @@
+import style from "./ToggleButton.scss";
+
+// 컴포넌트 사용시 받아올 값을 여기에 명시합니다.
+export interface IToggleButtonProps {
+  // 문자열 예시
+  // 숫자 예시
+  // age: number;
+}
+
+// 리액트 컴포넌트 코드를 여기에 입력합니다.
+const ToggleButton = (props: IToggleButtonProps) => {
+  // 자바스크립트 코드가 여기에 입력됩니다.
+  const styles = {
+    togglebutton: {
+      marginBottom: 16,
+    },
+  };
+  // HTML + React 코드가 여기 입력됩니다.
+  return (
+    <>
+      <div className="toggleButton">
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      {/* SCSS 스타일 파일 주입을 위한 코드입니다. 수정 X */}
+      <style jsx>{style}</style>
+    </>
+  );
+};
+
+export default ToggleButton;
