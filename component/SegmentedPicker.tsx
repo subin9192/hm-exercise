d74cf6c8aae33321e0d1d56854ca5081e72871b7
subin <subin9192@gmail.com>
2022-09-07 18:21:17 +0900

    feat: SegmentedPicker

---
+++ component/SegmentedPicker.tsx
@@ -0,0 +1,38 @@
+import style from "./SegmentedPicker.scss";
+
+// 컴포넌트 사용시 받아올 값을 여기에 명시합니다.
+export interface ISegmentedPickerProps {
+  // 문자열 예시
+  // 숫자 예시
+  // age: number;
+  repeat: number;
+
+  switch: boolean;
+}
+
+// 리액트 컴포넌트 코드를 여기에 입력합니다.
+const SegmentedPicker = (props: ISegmentedPickerProps) => {
+  // 자바스크립트 코드가 여기에 입력됩니다.
+  Array.from({ length: 4 }).map(() => {
+    // HTML + React 코드가 여기 입력됩니다.
+    return (
+      <>
+        <div className="segmentedPicker">
+          <div className="selection">
+              <input type="radio" id={props.switch} name="sample" value={props.switch} />
+                <label htmlFor={props.switch}>{props.switch ? "On" : "Off"}
+                  ::before
+                  <span>{props.switch ? "On" : "Off"}</span>
+                  ::after
+                </label>
+          </div>
+        </div>
+
+        {/* SCSS 스타일 파일 주입을 위한 코드입니다. 수정 X */}
+        <style jsx>{style}</style>
+      </>
+    );
+  });
+};
+
+export default SegmentedPicker;
