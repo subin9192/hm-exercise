6e6793a5c72f1d73227c32e87cba3768b53bc947
subin <subin9192@gmail.com>
2022-09-13 18:00:53 +0900

    feat: SegmentedPicker

--- component/SegmentedPicker.demo.tsx
+++ component/SegmentedPicker.demo.tsx
@@ -27,37 +27,11 @@ export default demo({
     //     selectableValues: ["English", "Korean"],
     //   },
     // },
-    id: {
-      type: "string",
-      defaultValue: "id",
-    },
-
-    label: {
-      type: "string",
-      defaultValue: "label",
-    },
-    
-    "data-on": {
-      type: "string",
-      defaultValue: "data-on",
-    },
-  
+    // * 불리언 예시
     isChecked: {
       type: "boolean",
       defaultValue: false,
     },
-  
-    "data-off": {
-      type: "string",
-      defaultValue: "data-off",
-    },
-  
-    description: {
-      type: "string",
-      defaultValue: "description",
-    },
-  
-    // * 불리언 예시
   },
 
   renderProps: (props) => {
