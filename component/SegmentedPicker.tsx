4a51b38f774679ab75fe862545ca15cc1eff4626
subin <subin9192@gmail.com>
2022-09-13 17:25:57 +0900

    feat: SegmentedPicker

--- component/SegmentedPicker.tsx
+++ component/SegmentedPicker.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import style from "./SegmentedPicker.scss";
 
-
 // 컴포넌트 사용시 받아올 값을 여기에 명시합니다.
 export interface ISegmentedPickerProps {
   // 문자열 예시
@@ -27,50 +26,46 @@ export interface ISegmentedPickerProps {
 // 리액트 컴포넌트 코드를 여기에 입력합니다.
 const SegmentedPicker = (props: ISegmentedPickerProps) => {
   // 자바스크립트 코드가 여기에 입력됩니다.
-  Array.from({ length: 4 }).map(() => {
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.onChange && props.onChange(event.target.checked);
-    };
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChange && props.onChange(event.target.checked);
+  };
 
-    const labelId = `label-${props.id}`;
-    const descriptionId = `description-${props.id}`;
+  const labelId = `label-${props.id}`;
+  const descriptionId = `description-${props.id}`;
 
-    const labelBy = labelId + " " + descriptionId;
-    // HTML + React 코드가 여기 입력됩니다
-    return (
-      <>
-        <div className="segmentedPicker" selectedIndex={props.repeat}>
-          <label htmlFor={props.id} className="switch">
-            <input
-              id={props.id}
-              type="checkbox"
-              role="switch"
-              data-on={props["data-on"]}
-              checked={props.isChecked}
-              data-off={props["data-off"]}
-              onChange={onChange}
-              aria-checked={props.isChecked}
-              aria-labelledby={labelBy}
-            />
-            <div className="switch-labels">
-              <span id={labelId}>{props.label}</span>
-              {props.description && (
-                <p id={descriptionId}>{props.description}</p>
-              )}
-            </div>
-          </label>{" "}
-        </div>
+  const labelBy = labelId + " " + descriptionId;
+  // HTML + React 코드가 여기 입력됩니다
+  return (
+    <>
+      <div className="segmentedPicker" selectedIndex={props.repeat}>
+        <label htmlFor={props.id} className="switch">
+          <input
+            id={props.id}
+            type="checkbox"
+            role="switch"
+            data-on={props["data-on"]}
+            checked={props.isChecked}
+            data-off={props["data-off"]}
+            onChange={onChange}
+            aria-checked={props.isChecked}
+            aria-labelledby={labelBy}
+          />
+          <div className="switch-labels">
+            <span id={labelId}>{props.label}</span>
+            {props.description && <p id={descriptionId}>{props.description}</p>}
+          </div>
+        </label>{" "}
+      </div>
 
-        {/* SCSS 스타일 파일 주입을 위한 코드입니다. 수정 X */}
-        <style jsx>{style}</style>
-      </>
-    );
-  });
+      {/* SCSS 스타일 파일 주입을 위한 코드입니다. 수정 X */}
+      <style jsx>{style}</style>
+    </>
+  );
 };
 
 SegmentedPicker.defaultProps = {
-  "data-on": 'ON',
-  "data-off": 'OFF'
-}
+  "data-on": "ON",
+  "data-off": "OFF",
+};
 
 export default SegmentedPicker;
